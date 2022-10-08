35edd2a903d405c938df4271e63160a27bf5f1d0
Enjoy3325 <enjoy3325@gmail.com>
2022-10-08 14:51:59 +0300

    commit-08-02-hw-js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,12 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
 
+player.on('timeupdate', throttle(getVideoTitle, 1000));
+
+function getVideoTitle(evt) {
+  localStorage.setItem('videoplayer-current-time', `${evt.seconds}`);
+}
+const currentTime = Number(localStorage.getItem('videoplayer-current-time'));
+currentTime ? player.setCurrentTime(currentTime) : '';
