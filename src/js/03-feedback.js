2480cebff752ef9260ad95f5801f6706c276cacf
Enjoy3325 <enjoy3325@gmail.com>
2022-10-11 14:04:58 +0300

    done-hw-08-03-js
    +

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,33 @@
+import throttle from 'lodash.throttle';
+const ref = {
+  form: document.querySelector('.feedback-form'),
+  email: document.querySelector('input'),
+  textarea: document.querySelector('textarea'),
+};
+const LOCALSTORAGE_KEY = 'feedback-form-state';
 
+ref.form.addEventListener('submit', onSubmitForm);
+ref.form.addEventListener('input', throttle(onFormInput, 500));
+
+const formData = {
+  email: '',
+  message: '',
+};
+
+function onFormInput(e) {
+  formData[e.target.name] = e.target.value;
+  localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(formData));
+}
+
+function onSubmitForm(e) {
+  e.preventDefault();
+  localStorage.removeItem(LOCALSTORAGE_KEY);
+  e.currentTarget.reset();
+}
+
+const data = JSON.parse(localStorage.getItem(LOCALSTORAGE_KEY));
+
+if (data) {
+  ref.email.value = data.email;
+  ref.textarea.value = data.message;
+}
